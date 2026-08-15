dadab9f1dcc1582f7660784121481a5b1c8e0f2c
rafaelferracini <121617955+rafaelferracini@users.noreply.github.com>
2026-08-15 19:07:16 -0300

    add config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+alias config="git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
