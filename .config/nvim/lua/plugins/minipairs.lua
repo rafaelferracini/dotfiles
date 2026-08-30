e9f9e56db1f06f738cbfa7cb8a4e6a48f37afe0f
Rafael Ferracini <rafaelhferracini@gmail.com>
2026-08-30 18:30:41 -0300

    feat(nvim): change mini.pairs to nvim-mini and add identscope

--- .config/nvim/lua/plugins/minipairs.lua
+++ .config/nvim/lua/plugins/minipairs.lua
@@ -1,14 +1,8 @@
 return {
-	"echasnovski/mini.pairs",
-	version = false, -- Opcional: Garante que você sempre use a versão mais recente do branch principal
-	event = "InsertEnter", -- Só carrega o plugin quando você começar a digitar algo (Modo Insert)
+	"nvim-mini/mini.nvim",
+	version = false,
 	config = function()
-		require("mini.pairs").setup({
-			-- O mini.pairs funciona perfeitamente sem nenhuma configuração extra,
-			-- mantendo a filosofia de ser leve e direto ao ponto.
-
-			-- Aqui você poderia mapear caracteres diferentes se quisesse,
-			-- mas o padrão já cobre (), [], {}, "", e ''.
-		})
+		require("mini.pairs").setup()
+		require("mini.indentscope").setup()
 	end,
 }
