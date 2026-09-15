d522eeec3151fcfedae2e15c6a3fcabc416419a6
Rafael Ferracini <rafaelhferracini@gmail.com>
2026-09-15 18:45:29 -0300

    fix(hypr): add NVIDEA settings

--- .config/hypr/config/monitors.lua
+++ .config/hypr/config/monitors.lua
@@ -8,15 +8,15 @@
 -- })
 
 hl.monitor({
-    output    = "HDMI-A-1",
-    mode      = "1920x1080@200",
-    position  = "0x0",
-    scale     = 1,
+	output = "HDMI-A-1",
+	mode = "1920x1080@200",
+	position = "0x0",
+	scale = 1,
 })
 
 hl.monitor({
-    output    = "eDP-1",
-    mode      = "1920x1080@144",
-    position  = "1920x0",
-    scale     = 1,
+	output = "eDP-1",
+	mode = "1920x1080@144",
+	position = "1920x0",
+	scale = 1,
 })
